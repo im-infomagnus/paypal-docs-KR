2edacf118b1031ad45dcfd1beeba4f4be6477626
KalpanaReddyC <33893559+KalpanaReddyC@users.noreply.github.com>
2023-06-21 17:48:45 +0000

    Updated server.js file in im-demo

--- lessons/im-demo/server.js
+++ lessons/im-demo/server.js
@@ -1,42 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import * as paypal from "./paypal-api.js";
-import fs from 'fs';
 const { PORT = 8888 } = process.env;
-
-// Read the devcontainer.json file
-const devcontainerData = fs.readFileSync('../../.devcontainer/im-demo/devcontainer.json');
-const devcontainerConfig = JSON.parse(devcontainerData);
-
-// Access the desired values from devcontainerConfig
-const value1 = devcontainerConfig.name;
-const value2 = devcontainerConfig.workspaceFolder;
-
-console.log(value1);
-console.log(value2);
-
-// Read the devcontainer.json file
-//const devcontainerData = fs.readFileSync('D:/GitHub_Projects/Paypal/paypal-docs-KR/.devcontainer/im-demo/devcontainer.json', 'utf8');
-
-// Read the devcontainer.json file
-//const devcontainerData = fs.readFileSync('./.devcontainer/im-demo/devcontainer.json', 'utf8');
-
-// Parse the JSON data
-const devcontainerJson = JSON.parse(devcontainerData);
-
-// Access the desired value from devcontainerJson
-const port = devcontainerJson['secrets']['CLIENT_ID'];
-
-// Use the port value
-console.log('Port:', port);
-
-
-
-// Read variables from devcontainer.json file
-// https://code.visualstudio.com/docs/remote/devcontainerjson-reference
-// https://code.visualstudio.com/docs/remote/containers-advanced#_adding-environment-variables
-// https://code.visualstudio.com/docs/remote/containers-advanced#_adding-environment-variables-to-a-container
-// https://code.visualstudio.com/docs/remote/containers-advanced#_using-a-docker-compose-file
 
 
 const app = express();
