e7fc891bf17954c9f25d6de0a28dcdc60065de05
Kalpana Reddy <Kalpana.c@infomagnus.com>
2023-06-21 23:19:39 +0530

    Reverted back static values for secret keys

--- lessons/advanced-integration/server.js
+++ lessons/advanced-integration/server.js
@@ -3,13 +3,14 @@ import express from "express";
 import * as paypal from "./paypal-api.js";
 const { PORT = 8888 } = process.env;
 
+
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 // render checkout page with client id & unique client token
 app.get("/", async (req, res) => {
-  const clientId = "AfO8JyqOwNtRMq-3X9jr583UkVF10hxeG9Ifku6354w4Xh6eNOSClKl_6lLGi8FEDxseWsDwd9TdmGFG";
+  const clientId = process.env.CLIENT_ID;
   try {
     const clientToken = await paypal.generateClientToken();
     res.render("checkout", { clientId, clientToken });
